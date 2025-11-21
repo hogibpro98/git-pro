231a7e407e0b2a0be59526ca08b08432e233402f
Ninja Vue <hont.fsd@gmail.com>
2025-11-21 17:34:54 +0700

    Update total amount displayed in order index

--- order/index.php
+++ order/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once 'order.php';
 
-echo "<h1>Total " . formatCurrency(2010) . "</h1>";
+echo "<h1>Total " . formatCurrency(20109999) . "</h1>";
 echo "<p>Current Version: " . APP_VERSION . "</p>";
