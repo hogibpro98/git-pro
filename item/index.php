847ff0ca197717eff770094cc381bff54a3de8cb
Ninja Vue <hont.fsd@gmail.com>
2025-11-22 15:26:21 +0700

    feature/No2-ca-item-main
    (cherry picked from commit 138821e70d31b4a8f9edf12f911bae1b0389998b)

--- item/index.php
+++ item/index.php
@@ -4,3 +4,8 @@ require_once 'order.php';
 echo "<h1>Total " . formatCurrency(100) . "</h1>";
 echo "<p>Current Version: " . APP_VERSION . "</p>";
 echo "<p>Sum: " . sum(10, 20) . "</p>";
+
+// Additional code can be added here if needed
+echo "<p>Thank you for using our service!</p>";
+echo "<p>Contact us for more information.</p>";
+echo "<p>Have a great day!</p>";
